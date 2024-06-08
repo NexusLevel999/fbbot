11bf2881d67eb09a3bcd42146105176527c8061b
NexusLevel999 <nexuskaizu@gmail.com>
2024-06-08 14:24:36 +0800

    .

---
+++ modules/announce.js
@@ -0,0 +1,77 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+
+module.exports.config = {
+	name: "announce",
+	access: 1,
+	info: "Sends a message to all groups and can only be done by the admin.",
+	needPrefix: false,
+	alts: ["noti","notice","announce"],
+	usage: "[Text]",
+	cd: 0,
+};
+
+module.exports.run = async function ({ api, event, args, admin }) {
+	const threadList = await api.getThreadList(100, null, ["INBOX"]);
+	let sentCount = 0;
+	const custom = args.join(" ");
+
+	async function sendMessage(thread) {
+		try {
+			await api.sendMessage(
+`NOTICE FROM THE ADMIN:\n ${custom}`,
+				thread.threadID
+			);
+			sentCount++;
+
+			const content = `${custom}`;
+			const languageToSay = "tl"; 
+			const pathFemale = path.resolve(__dirname, "cache", `${thread.threadID}_female.mp3`);
+
+			await downloadFile(
+				`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(content)}&tl=${languageToSay}&client=tw-ob&idx=1`,
+				pathFemale
+		);
+			api.sendMessage(
+				{ attachment: fs.createReadStream(pathFemale) },
+				thread.threadID,
+				() => fs.unlinkSync(pathFemale)
+			);
+		} catch (error) {
+			console.error("Error sending a message:", error);
+		}
+	}
+
+	for (const thread of threadList) {
+		if (sentCount >= 20) {
+			break;
+		}
+		if (thread.isGroup && thread.name != thread.threadID && thread.threadID != event.threadID) {
+			await sendMessage(thread);
+		}
+	}
+
+	if (sentCount > 0) {
+		api.sendMessage(`› Sent the notification successfully.`, event.threadID);
+	} else {
+		api.sendMessage(
+			"› No eligible group threads found to send the message to.",
+			event.threadID
+		);
+	}
+};
+
+async function downloadFile(url, filePath) {
+	const writer = fs.createWriteStream(filePath);
+	const response = await axios({
+		url,
+		method: 'GET',
+		responseType: 'stream'
+	});
+	response.data.pipe(writer);
+	return new Promise((resolve, reject) => {
+		writer.on('finish', resolve);
+		writer.on('error', reject);
+	});
+}
