39527b6554de4a2646058d543ee69dfd2f44d0ca
NexusLevel999 <nexuskaizu@gmail.com>
2024-06-08 14:14:27 +0800

    .

---
+++ modules/filter.js
@@ -0,0 +1,52 @@
+module.exports.config = {
+		name: "filter",
+		access: 2,
+		alts: ["rmfbuser"]
+		info: "Filter Faceboook User",
+		needPrefix: false,
+		usages: "",
+		cd: 5
+};
+
+module.exports.run = async function ({ api, event }) {
+		const { userInfo, adminIDs } = await api.getThreadInfo(event.threadID);
+		let successCount = 0;
+		let failCount = 0;
+		const filteredUsers = [];
+
+		for (const user of userInfo) {
+				if (user.gender === undefined) {
+						filteredUsers.push(user.id);
+				}
+		}
+
+		const isBotAdmin = adminIDs.map(a => a.id).includes(api.getCurrentUserID());
+
+		if (filteredUsers.length === 0) {
+				api.sendMessage("Your group does not exist 'Facebook User'.", event.threadID);
+		} else {
+				api.sendMessage(`Filtering group of friends ${filteredUsers.length} 'Facebook users'.`, event.threadID, () => {
+						if (isBotAdmin) {
+								api.sendMessage("Starting filtering...\n\n", event.threadID, async () => {
+										for (const userID of filteredUsers) {
+												try {
+														await new Promise(resolve => setTimeout(resolve, 1000));
+														await api.removeUserFromGroup(parseInt(userID), event.threadID);
+														successCount++;
+												} catch (error) {
+														failCount++;
+												}
+										}
+
+										api.sendMessage(`Successfully filtered ${successCount} people.`, event.threadID, () => {
+												if (failCount !== 0) {
+														api.sendMessage(`❌ Failed to filter ${failCount} people.`, event.threadID);
+												}
+										});
+								});
+						} else {
+								api.sendMessage("Bot is not an admin, so it can't filter.", event.threadID);
+						}
+				});
+		}
+};
